362487f6ef6ae727e1fabe6afbd34554a521bdd4
snowmi <trouvelot@gmail.com>
2015-05-06 23:40:50 +0900

    Improve minitest setting

--- test/test_ITP1_1_A.rb
+++ test/test_ITP1_1_A.rb
@@ -1,10 +1,11 @@
-require "minitest/unit"
 require "minitest/autorun"
+require "minitest/reporters"
 require "../Problems/ITP1/ITP1_1_A"
 
-MiniTest::Unit.autorun
+MiniTest.autorun
+Minitest::Reporters.use!
 
-class TestHello < MiniTest::Unit::TestCase
+class TestHello < MiniTest::Test
   def test_hello_world
     assert_equal "Hello World", Hello.hello_world
   end
