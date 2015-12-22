a4bb7033c905b0965ddfcb2b170874bbf2d71c33
snowmi <trouvelot@gmail.com>
2015-12-23 00:14:55 +0900

    add ITP1_11_A

---
+++ Problems/ITP1/ITP1_11_A.rb
@@ -0,0 +1,19 @@
+dice = []
+dice = gets.chomp.split
+
+directions = gets.chomp
+
+directions.length.times do |i|
+  case direction[i]
+  when "S"
+    #
+  when "E"
+    #
+  when "N"
+    #
+  when "W"
+    #
+  end
+end
+
+puts dice[n]
